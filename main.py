60629e6b15dd3b65b685438a68694744c677f1a1
kunningKing11 <grlicanikolai@gmail.com>
2026-04-06 14:02:30 +0200

    feat: add clock object and dt variable

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
